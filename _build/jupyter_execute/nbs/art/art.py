fe045149c97098b7847ec92e8ff60f7393b99a37
cuteopamp <edayilmxz@outlook.com>
2023-02-10 16:10:55 +0300

    a

---
+++ _build/jupyter_execute/nbs/art/art.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ```{figure} ../../assets/anyalast.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 01. Summer Mood
+# ```
+# 
+# ```{figure} ../../assets/dragon.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 02. Guide Your Own Way
+# ```
+# 
+# ```{figure} ../../assets/last.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 03. Love Bird
+# ```
+# 
+# ```{figure} ../../assets/renkliel.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 04. Reaching to You
+# ```
+# 
+# ```{figure} ../../assets/flowergirl2.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 05. Night Struggle
+# ```
+# 
+# ```{figure} ../../assets/rengokulastbutveryveryvery.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 06. Blaze Your Heart
+# ```
+# 
+# ```{figure} ../../assets/habibi.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 07. Tamino
+# ```
+# 
+# ```{figure} ../../assets/pychann.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 08. Python-chan
+# ```
+# 
+# ```{figure} ../../assets/bmo.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 09. Time of Adventure
+# ```
+# 
+# ```{figure} ../../assets/pythonchan2.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 10. Desert of Python-chan
+# ```
+# 
+# ```{figure} ../../assets/dgkosenpai.png
+# ---
+# height: 512px
+# name: directive-fig
+# ---
+# Art 10. Twinkle Twinkle
+# ```
+
+# 
